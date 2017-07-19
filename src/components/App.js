ce8945e61e538b2e15aa269a8c669d3775354138
dwelby101 <dwelby101@msn.com>
2017-07-20 00:36:36 +0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+// Import widgets being used in this component
+import NumberWidgetContainer from '../components/NumberWidgetContainer';
+import ListWidgetContainer from '../components/ListWidgetContainer';
+import GraphWidgetContainer from '../components/GraphWidgetContainer';
+
+// Add in styles
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <ListWidgetContainer
+                    href="http://localhost:3001/stats/top"
+                    heading="Top Ticket Answerers"
+                    rowspan={3} />
+
+                <NumberWidgetContainer
+                    href="http://localhost:3001/tickets/open"
+                    heading="Open Ticket Total" />
+
+                <GraphWidgetContainer
+                    href="http://localhost:3001/tickets/progression"
+                    heading="Tickets Over Time"
+                    colspan={2} rowspan={2} />
+
+                <NumberWidgetContainer
+                    href="http://localhost:3001/tickets/today"
+                    heading="Tickets Opened Today" />
+
+                <NumberWidgetContainer
+                    href="http://localhost:3001/tickets/urgent"
+                    heading="Tickets Marked 'Urgent'" />
+
+                <NumberWidgetContainer
+                    href="http://localhost:3001/stats/response"
+                    heading="4 Hour Response %"
+                    colspan={2} />
+            </div>
+        );
+    }
+}
+
+export default App;
